e5c996232fd8c10b37a3a4dec9b7bf127dd41bfc
S4kyt <jerikolo1@gmail.com>
2020-12-01 14:58:06 +0100

    Standard Exceptations

--- src/section-2/standardExceptions.cpp
+++ src/section-2/standardExceptions.cpp
@@ -4,9 +4,29 @@
 
 #include <iostream>
 
+class CanGoWrong {
+public:
+	CanGoWrong() {
+		// Allocating memory
+		// We'd get the exception here in case allocating a chunk of memory thats too big, but I am using Visual Studio which doesn't even let me build the program like this.
+		char* pMemory = new char[999999999];
+		delete[] pMemory;
+	}
+};
+
 void standardExceptions() {
 
-	std::cout << "Standard Exceptions!" << std::endl;
+	try {
+		CanGoWrong wrong;
+	}
+	// bad_alloc class object
+	// Thats why we use reference here
+	catch (std::bad_alloc &e) {
+		// bad_alloc.what() prints out an useful message to us
+		std::cout << "Caught exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "Still running." << std::endl;
 
 	toContinue();
 }
