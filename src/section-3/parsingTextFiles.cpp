d357cb89ea523e43a250543a9d4f9075a8a119d9
S4kyt <jerikolo1@gmail.com>
2020-12-02 14:58:30 +0100

    Parsing Text Files

--- src/section-3/parsingTextFiles.cpp
+++ src/section-3/parsingTextFiles.cpp
@@ -1,11 +1,46 @@
 // Parsing Text Files
 
 #include <iostream>
-
+#include <fstream>
+#include <string>
 #include "../util.h"
 
 void parsingTextFiles() {
-	std::cout << "Parsing Text Files" << std::endl;
+
+	std::string filename = "src/section-3/stats.txt";
+	std::ifstream input;
+
+	input.open(filename);
+
+	if (!input.is_open()) {
+		return;
+	}
+
+	while (input) {
+		std::string line;
+
+		std::getline(input, line, ':');
+
+		int population;
+
+		// extraction operator
+		input >> population;
+
+		// discards empty spaces
+		// input.get();
+
+		// read "white space"
+		input >> std::ws;
+
+
+		//if (!input) {
+		//	break;
+		//}
+
+		std::cout << "'" << line << "'" << " -- " << "'" << population << "'" << std::endl;
+	}
+
+	input.close();
 
 	toContinue();
 }
