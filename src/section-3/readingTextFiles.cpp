765298a92a24d64fa71d1720f157878fb1085392
S4kyt <jerikolo1@gmail.com>
2020-12-02 14:44:25 +0100

    Reading Text Files

--- src/section-3/readingTextFiles.cpp
+++ src/section-3/readingTextFiles.cpp
@@ -1,13 +1,37 @@
 // Reading Text Files
 
 #include <iostream>
+#include <fstream>
+// getline()
+#include <string>
 
 #include "../util.h"
 
 void readingTextFiles() {
 	
+	std::string inFileName = "src/section-3/writingReading.txt";
 
-	//"src/section-3/writingReading.txt";
+	std::ifstream inFile;
+
+	inFile.open(inFileName);
+
+	if (inFile.is_open()) {
+		
+		std::string line;
+
+		// Read line-by line
+		while (inFile) {
+			std::getline(inFile, line);
+			std::cout << line;
+		}
+
+		std::cout << line;
+
+		inFile.close();
+	}
+	else {
+		std::cout << "Cannot open file: " << inFileName << std::endl;
+	}
 
 
 	toContinue();
