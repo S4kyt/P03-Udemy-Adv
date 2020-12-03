99c0c009f42f02b24ad5cdf499468d0f3b6baa40
S4kyt <jerikolo1@gmail.com>
2020-12-03 15:29:06 +0100

    Vectors

--- src/section-4/vectorsPractice.cpp
+++ src/section-4/vectorsPractice.cpp
@@ -1,12 +1,35 @@
 // Vectors
 
 #include <iostream>
+#include <vector>
 
 #include "../util.h"
 
 void vectorsPractice() {
 
-	std::cout << "Vectors" << std::endl;
+	// declaring vector object, <int> being its type
+	std::vector<std::string> strings;
+
+	// adding 3 elements to the vector
+	strings.push_back("one");
+	strings.push_back("two");
+	strings.push_back("three");
+
+	// looping through with normally
+	for (int i = 0; i < strings.size(); i++) {
+		std::cout << strings[i] << std::endl;
+	}
+
+	// looping through with iterator
+	for (std::vector<std::string>::iterator it = strings.begin(); it != strings.end(); it++) {
+		std::cout << *it << std::endl;
+	}
+
+	// pointing to element 3
+	std::vector<std::string>::iterator it = strings.begin();
+	it += 2;
+
+	std::cout << *it << std::endl;
 
 	toContinue();
 }
